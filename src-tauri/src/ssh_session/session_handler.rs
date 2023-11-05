478088df6b6b3cc91100c4516f3760981e668840
Grilo16 <tbritton@dufrain.co.uk>
2023-11-05 22:56:34 +0000

    tester

--- src-tauri/src/ssh_session/session_handler.rs
+++ src-tauri/src/ssh_session/session_handler.rs
@@ -13,7 +13,7 @@ pub fn create_session(host_param: &String) -> Result<Session, Error> {
     // let username = "grilo16";
     // let password = "B0oPkkemMakjDA";
     // ec2-35-178-172-93.eu-west-2.compute.amazonaws.com
-    let host_param_cleaned = host_param.replace("\n", "");
+    let host_param_cleaned = host_param.replace("\n", "").replace("#", "");
     let host_param_cleaned_str = host_param_cleaned.trim().to_string(); // Convert to String;
     let host = format!("{}:22", host_param_cleaned_str);
     println!("{}", host);
