199c45a19f92a42c0e00ca80377cbbf1e07f63f8
Grilo16 <tbritton@dufrain.co.uk>
2023-11-06 19:55:01 +0000

    just in case

---
+++ src-tauri/src/db/models/ssh_credentials.rs
@@ -0,0 +1,68 @@
+use regex::Regex;
+use serde::Serialize;
+use rusqlite::{types::{FromSql, ValueRef, FromSqlError}};
+use std::fmt;
+
+#[derive(Debug, Serialize)]
+pub struct SshCredentials {
+    pub id: i64,
+    pub username: String,
+    pub host: String,
+    pub ssh_command: String,
+}
+
+impl SshCredentials {
+    pub fn new(id: i64, username: String, host: String, ssh_command: String) -> Self {
+        SshCredentials {
+            id,
+            username,
+            host,
+            ssh_command,
+        }
+    }
+
+    pub fn from_ssh_string(string : &str) -> Option<SshCredentials> {
+        let (username, host) = Self::parse_username_host(string)?;
+        let id = 0;
+        let ssh_command = format!("-ssh {}@{}", username, host);
+        Some(Self::new(id, username, host, ssh_command))
+    }
+
+    pub fn parse_username_host(string: &str) -> Option<(String, String)> {
+        let re = Regex::new(r"([^@]+)@([^ ]+.com)").unwrap();
+        if let Some(credentials) = re.captures(string) {
+            let username = credentials[1].to_string();
+            let host = credentials[2].to_string();
+            Some((username, host))
+        } else {
+            None
+        }
+    } 
+
+}
+
+impl fmt::Display for SshCredentials {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "id: {}, username: {}, host: {}, ssh_command: {}",
+            self.id, self.username, self.host, self.ssh_command
+        )
+    }
+}
+
+impl FromSql for SshCredentials {
+    fn column_result(value: ValueRef) -> Result<SshCredentials, FromSqlError> {
+        let id: i64 = value.as_i64()?;
+        let username: String = value.as_str()?.to_string();
+        let host: String =  value.as_str()?.to_string();
+        let ssh_command: String =  value.as_str()?.to_string();
+
+        Ok(SshCredentials {
+            id,
+            username,
+            host,
+            ssh_command,
+        })
+    }
+}
---
+++ src-tauri/src/db/repositories/ssh_cred_repo.rs
@@ -0,0 +1,90 @@
+use crate::SshCredentials;
+use crate::Database;
+use rusqlite::{Connection, Result, Error, params};
+
+pub struct SshCredentialsRepo {
+    conn: Connection,
+}
+
+impl SshCredentialsRepo {
+    pub fn new() -> Self {
+        let db = Database::open().expect("Failed to open database");
+        Self { conn: db.get_connection() }
+    }
+
+    pub fn insert_ssh_credentials(&self, credentials: &SshCredentials) -> Result<(), Error> {
+        self.conn.execute(
+            "INSERT INTO ssh_credentials (username, host, ssh_command)
+             VALUES (?1, ?2, ?3)",
+            params![&credentials.username, &credentials.host, &credentials.ssh_command],
+        )?;
+        Ok(())
+    }
+
+    pub fn get_ssh_credentials(&self, id: i64) -> Result<Option<SshCredentials>, Error> {
+        let mut stmt = self.conn.prepare("SELECT * FROM ssh_credentials WHERE id = ?1")?;
+        let credentials = stmt.query_row(params![id], |row| {
+            Ok(SshCredentials {
+                id: row.get(0)?,
+                username: row.get(1)?,
+                host: row.get(2)?,
+                ssh_command: row.get(3)?,
+            })
+        });
+
+        match credentials {
+            Ok(credentials) => Ok(Some(credentials)),
+            Err(rusqlite::Error::QueryReturnedNoRows) => Ok(None),
+            Err(err) => Err(err),
+        }
+    }
+
+    pub fn get_all_ssh_credentials(&self) -> Result<Vec<SshCredentials>, Error> {
+        let mut stmt = self.conn.prepare("SELECT * FROM ssh_credentials")?;
+        let credentials_iter = stmt.query_map([], |row| {
+            Ok(SshCredentials {
+                id: row.get(0)?,
+                username: row.get(1)?,
+                host: row.get(2)?,
+                ssh_command: row.get(3)?,
+            })
+        })?;
+
+        let mut credentials = Vec::new();
+
+        for credential in credentials_iter {
+            match credential {
+                Ok(credential) => {
+                    credentials.push(credential);
+                }
+                Err(err) => {
+                    return Err(err);
+                }
+            }
+        }
+
+        Ok(credentials)
+    }
+
+    pub fn update_ssh_credentials(&self, credentials: &SshCredentials) -> Result<(), Error> {
+        self.conn.execute(
+            "UPDATE ssh_credentials
+             SET username = ?2, host = ?3, ssh_command = ?4
+             WHERE id = ?1",
+            params![credentials.id, &credentials.username, &credentials.host, &credentials.ssh_command],
+        )?;
+        Ok(())
+    }
+
+    pub fn delete_ssh_credentials(&self, id: i64) -> Result<(), Error> {
+        let result = self.conn.execute("DELETE FROM ssh_credentials WHERE id = ?1", params![id]);
+
+        match result {
+            Ok(_) => Ok(()),
+            Err(err) => {
+                eprintln!("Error deleting SSH credentials: {:?}", err);
+                Err(err)
+            }
+        }
+    }
+}
